533039dd9f7c8bdb61c66e51ed97c3e26760edde
Thigos <dev.findviewbyid@gmail.com>
2023-11-26 20:44:44 -0300

    Mysql && SQL Server Log

---
+++ DBConnection.py
@@ -0,0 +1,39 @@
+import mysql.connector
+import pyodbc
+import json
+
+class DBConnection:
+    def __init__(self):
+        self.__connectionMysql = mysql.connector.connect(
+            host='localhost',
+            database='streamoon',
+            user='StreamoonUser',
+            password='Moon2023'
+        )
+
+        self.__connectionSQLServer = pyodbc.connect(
+            'DRIVER={SQL Server};'
+            'SERVER=127.0.0.1;'
+            'DATABASE=streamoon;'
+            'UID=StreamoonUser;'
+            'PWD=Moon2023;'
+            'TrustServerCertificate=yes;'
+        )
+
+    def writeDB(self, dictData : dict, idUsuario: int, isBot: bool):
+        jsonstr = json.dumps(dictData, ensure_ascii=False)
+        instrucao = f"INSERT INTO logMoonAssistant (fkUsuario, msg, isBot) VALUES ({idUsuario}, '{jsonstr}', {int(isBot)});"
+
+        #SQL Server Insety
+        cursor = self.__connectionSQLServer.cursor()
+        cursor.execute(instrucao)
+        self.__connectionSQLServer.commit()
+        cursor.close()
+
+        #Mysql Insert
+        cursor = self.__connectionMysql.cursor()
+        cursor.execute(instrucao)
+
+        self.__connectionMysql.commit()
+        cursor.close()
+
--- localServer.py
+++ localServer.py
@@ -1,22 +1,34 @@
 from flask import Flask, jsonify, request
 from MoonAssistant import MoonAssistant
+from DBConnection import DBConnection
 
 app = Flask(__name__)
 moonAssistant = MoonAssistant()
+dbConnection = DBConnection()
 
 @app.route('/MoonAssistant/send', methods=['POST'])
-def hello():
+def send():
     data = request.get_json()
-    
     message = data['message']
+    idUsuario = data['idUsuario']
 
     response = moonAssistant.send(message)
 
-    return jsonify({
+    json = {
         'content':response['content'],
         'images':response['images'] if 'images' in response.keys() else [],
         'links':response['links'] if 'links' in response.keys() else [],
-    })
+    }
+
+    dbConnection.writeDB({
+        'content': message,
+        'images': [],
+        'links': [],
+    }, idUsuario, False)
+
+    dbConnection.writeDB(json, idUsuario, True)
+
+    return jsonify(json)
 
 if __name__ == '__main__':
     app.run(debug=False, port=5555)
